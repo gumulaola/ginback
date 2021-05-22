db74f9a6187ba343e05c014af35495f383d2c76a
xfeng96 <fx96@stu.pku.edu.cn>
2021-05-22 17:11:38 +0800

    update main

--- main.go
+++ main.go
@@ -8,25 +8,4 @@ import (
 func main() {
 	r := routers.InitRouter()
 	r.Run()
-
-	// r := gin.Default()
-
-	// index
-	// r.GET("/", func(c *gin.Context) {
-	// 	c.JSON(200, gin.H{
-	// 		"message": "Hello",
-	// 	})
-	// })
-
-	// upload
-	// r.POST("/upload", func(c *gin.Context) {
-	// 	file, _ := c.FormFile("file")
-	// 	log.Println(file.Filename)
-	// 	filePath := "upload/"
-	// 	path := filePath + file.Filename
-	// 	c.SaveUploadedFile(file, path)
-	// 	c.String(http.StatusOK, fmt.Sprintf("uploaded!"))
-	// })
-
-	// r.Run()
 }
